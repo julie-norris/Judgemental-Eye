534fb597897a5b40ce740368474f64d4ed4176c4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-02 13:01:22 -0700

    created Flask routes and added Jinja html pages

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, render_template, redirect, request, 
+                    flash, session, jsonify)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -21,10 +22,16 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def index():
-    """Homepage."""
-    a = jsonify([1,3])
-    return a
+  """ Homepage """
 
+  return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
